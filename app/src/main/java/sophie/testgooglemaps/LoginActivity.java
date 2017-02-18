9897b56e31c7d085d2f241a362265433ddf6c429
Alex <alexandreguilngar@gmail.com>
2017-02-18 17:18:57 +0100

    Connection to the database (JSONParser + AsyncTask)

--- app/src/main/java/sophie/testgooglemaps/LoginActivity.java
+++ app/src/main/java/sophie/testgooglemaps/LoginActivity.java
@@ -2,19 +2,20 @@ package sophie.testgooglemaps;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -23,36 +24,50 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.protocol.HTTP;
-import org.xml.sax.Attributes;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
 import android.app.Activity;
+import android.util.Log;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Looper;
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import static java.sql.DriverManager.getConnection;
 
 /**
  * Created by Alex on 16/02/2017.
@@ -63,9 +78,10 @@ public class LoginActivity extends Activity {
     // Lien vers votre page php sur votre serveur
     private static final String	UPDATE_URL	= "https://glacial-falls-56009.herokuapp.com/register";
 
-    public ProgressDialog				progressDialog;
+    public ProgressDialog progressDialog;
 
-    private EditText						UserEditText;
+
+    private EditText UserEditText;
 
     public void onCreate(Bundle savedInstanceState)
     {
@@ -81,90 +97,57 @@ public class LoginActivity extends Activity {
             public void onClick(View v){
                 Log.i("Sophie_the_AI", "Ca marche");
 
+               /* List<NameValuePair> params = new ArrayList<NameValuePair>();
+                JSONParser jsonParser = new JSONParser();
+                Log.i("Sophie_the_AI", "Etape 2");
 
 
+                jsonParser.makeHttpRequest ("https://glacial-falls-56009.herokuapp.com/testdb?info=test", "GET", params);
 
-                /*// Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(this);
-                String url ="http://www.google.com";
+                Log.i("Sophie_the_AI", "Requete json faite");*/
 
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                mTextView.setText("Response is: "+ response.substring(0,500));
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        mTextView.setText("That didn't work!");
-                    }
-                });
-                // Add the request to the RequestQueue.
-                queue.add(stringRequest);*/
+                new JSONParse().execute();
 
-               try {
-                    URL url = new URL(UPDATE_URL);
-                    HttpURLConnection client = (HttpURLConnection) url.openConnection();
-                   // InputStream stream = connection.getInputStream();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                catch(java.io.IOException e){
-                    e.printStackTrace();
-                }
+                Log.i("Sophie_the_AI", "Requete json faite");
 
-
-              /* HttpURLConnection client = (HttpURLConnection) url.openConnection();
-
-                client.setRequestMethode("POST");
-                client.setRequestProperty("Key","Value");
-                client.setDoOutput(true);
-                */
-
-                /*HttpClient httpClient = new DefaultHttpClient();
-
-                // Create an object of HttpPost
-
-                HttpPost httpPost = new HttpPost(UPDATE_URL);
-
-                // Add POST parameters
-
-                List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(2);
-                nameValuePair.add(new BasicNameValuePair("username", "test_user"));
-                nameValuePair.add(new BasicNameValuePair("password", "123456789"));
-
-                //Encoding POST data
-                try {
-                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
-                } catch (UnsupportedEncodingException e) {
-                    // log exception
-                    e.printStackTrace();
-                }
-
-                //making POST request.
-                try {
-                    HttpResponse response = httpClient.execute(httpPost);
-                    // write response to log
-                    Log.d("Http Post Response:", response.toString());
-                } catch (ClientProtocolException e) {
-                    // Log exception
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    // Log exception
-                    e.printStackTrace();
-                }*/
-
-               Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
-               startActivity(myIntent);
+             // Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+              //startActivity(myIntent);
 
 
 
             }
         });
 
+    }
+
+    private class JSONParse extends AsyncTask<String, String, JSONObject> {
+        private ProgressDialog pDialog;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Getting Data ...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... args) {
+            JSONParser jsonParser = new JSONParser();
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // Getting JSON from URL
+            JSONObject json = jsonParser.makeHttpRequest ("https://glacial-falls-56009.herokuapp.com/testdb?info=test", "GET", params);
+            return json;
+        }
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            pDialog.dismiss();
+
+
+        }
     }
 
 }
