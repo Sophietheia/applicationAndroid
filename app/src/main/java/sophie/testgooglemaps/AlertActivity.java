d11c4d73ff1e0bae2a7a57827b9a0f1bdf88c01a
Alex <alexandreguilngar@gmail.com>
2017-03-06 12:01:31 +0100

    Inactivity notification

--- app/src/main/java/sophie/testgooglemaps/AlertActivity.java
+++ app/src/main/java/sophie/testgooglemaps/AlertActivity.java
@@ -1,9 +1,16 @@
 package sophie.testgooglemaps;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +34,8 @@ public class AlertActivity  extends Activity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
+
+
 
         Button myButton = (Button) findViewById(R.id.button4);
         myButton.setOnClickListener(new View.OnClickListener(){
