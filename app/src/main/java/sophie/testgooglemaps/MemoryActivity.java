c013b41aa71f4489318a18574d341e28278faef6
EmnaBouz <ebouzouita@gmail.com>
2017-03-06 15:59:01 +0100

    jeu specifique stade de maladie

--- app/src/main/java/sophie/testgooglemaps/MemoryActivity.java
+++ app/src/main/java/sophie/testgooglemaps/MemoryActivity.java
@@ -1,8 +1,10 @@
 package sophie.testgooglemaps;
 
+import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.app.Activity;
 import android.os.Bundle;
@@ -10,6 +12,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 /**
@@ -19,7 +28,7 @@ import android.widget.TextView;
 public class MemoryActivity extends Activity {
 
 
-
+    private static final String	UPDATE_URL	= "https://sophietheai.herokuapp.com/memory";
 
     private String hardgame="";
     private String easygame="";
@@ -39,18 +48,90 @@ public class MemoryActivity extends Activity {
             @Override
             public void onClick(View view){
 
-                try {
+               try {
+                    JSONObject response = new JSONParse().execute().get(); // On rajouter .get() à la fin pour récupérer le JSONObject qu'on return avec la méthode doInBackground
+                     System.out.println("hello");
+                    String connection = response.optString("stade");
+                    System.out.println(connection);  // Pour vérifier la valeure de connection dans les logs
+
+                   if (connection=="true")
+                   {
+
+                       try {
+                           Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(easygame));
+                           startActivity(browserIntent); }
+
+                       catch (ActivityNotFoundException e) {
+//                    Toast.makeText(this, "No application can handle this request." + " Please install a webbrowser",  Toast.LENGTH_LONG).show();
+                           e.printStackTrace();
+                       }
+
+                   }
+                   if (connection=="false")
+                   {
+
+                       try {
+                           Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(hardgame));
+                           startActivity(browserIntent); }
+
+                       catch (ActivityNotFoundException e) {
+//                    Toast.makeText(this, "No application can handle this request." + " Please install a webbrowser",  Toast.LENGTH_LONG).show();
+                           e.printStackTrace();
+                       }
+
+                   }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+        /*        try {
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(hardgame));
                 startActivity(browserIntent); }
 
                 catch (ActivityNotFoundException e) {
 //                    Toast.makeText(this, "No application can handle this request." + " Please install a webbrowser",  Toast.LENGTH_LONG).show();
                     e.printStackTrace();
-                }
+                }*/
 
 
             }
         });
 
     }
-}
+
+    private class JSONParse extends AsyncTask<String, String, JSONObject> {
+        private ProgressDialog pDialog;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pDialog = new ProgressDialog(MemoryActivity.this);
+            pDialog.setMessage("Getting Data ...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... args) {
+            JSONParser jsonParser = new JSONParser();
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // Getting JSON from URL
+            JSONObject json = jsonParser.makeHttpRequest (UPDATE_URL, "POST", params);
+
+            Log.i("Sophie_the_AI", json.toString());
+            return json;
+        }
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            pDialog.dismiss();
+
+
+
+        }
+    }
+
+
+    }
