7568f340d4a4730700a3007f1415704dab4b2ad8
tanohzana <tanohzana@gmail.com>
2017-03-07 18:40:06 +0100

    working on the reminders

--- app/src/main/java/sophie/testgooglemaps/RappelsActivity.java
+++ app/src/main/java/sophie/testgooglemaps/RappelsActivity.java
@@ -7,6 +7,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.apache.http.NameValuePair;
@@ -31,34 +33,45 @@ public class RappelsActivity extends AppCompatActivity {
 //    private TextView tv6;
 //    private TextView tv7;
 //    private TextView tv8;
-    private TextView tv9;
+//    private TextView tv9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rappels);
 
-        TextView tv_table[] = new TextView[10];
-
-        tv_table[1] = (TextView) findViewById(R.id.tv1);
-        tv_table[2] = (TextView) findViewById(R.id.tv2);
-        tv_table[3] = (TextView) findViewById(R.id.tv3);
-        tv_table[4] = (TextView) findViewById(R.id.tv4);
-        tv_table[5] = (TextView) findViewById(R.id.tv5);
-        tv_table[6] = (TextView) findViewById(R.id.tv6);
-        tv_table[7] = (TextView) findViewById(R.id.tv7);
-        tv_table[8] = (TextView) findViewById(R.id.tv8);
-        tv_table[9] = (TextView) findViewById(R.id.tv9);
+        String list[] = new String[10];
+//        TextView tv_table[] = new TextView[10];
+//
+//        tv_table[1] = (TextView) findViewById(R.id.tv1);
+//        tv_table[2] = (TextView) findViewById(R.id.tv2);
+//        tv_table[3] = (TextView) findViewById(R.id.tv3);
+//        tv_table[4] = (TextView) findViewById(R.id.tv4);
+//        tv_table[5] = (TextView) findViewById(R.id.tv5);
+//        tv_table[6] = (TextView) findViewById(R.id.tv6);
+//        tv_table[7] = (TextView) findViewById(R.id.tv7);
+//        tv_table[8] = (TextView) findViewById(R.id.tv8);
+//        tv_table[9] = (TextView) findViewById(R.id.tv9);
 
         Log.i("Sophie_the_AI", "on Create");
 
         try {
             JSONObject response = new RappelsActivity.JSONParse().execute().get();
             JSONArray arr = response.getJSONArray("json");
-            List<String> list = new ArrayList<String>();
+
             for(int i = 0; i < arr.length() && i<10; i++){
-                tv_table[i+1].setText(arr.getJSONObject(i).getString("name")+" le "+arr.getJSONObject(i).getString("date_task"));
+                list[i] = arr.getJSONObject(i).getString("name")+" le "+arr.getJSONObject(i).getString("date_task");
             }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, list);
+
+            ListView listView = (ListView) findViewById(R.id.list_reminders);
+            listView.setAdapter(adapter);
+//            List<String> list = new ArrayList<String>();
+//            for(int i = 0; i < arr.length() && i<10; i++){
+//                tv_table[i+1].setText(arr.getJSONObject(i).getString("name")+" le "+arr.getJSONObject(i).getString("date_task"));
+//            }
         } catch (Exception e) {
             e.printStackTrace();
         }
